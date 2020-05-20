39ce2f41d8d9e33b73149182638b4b781a2149bc
MCBauer <mikecbauervision@gmail.com>
2020-05-20 10:17:11 -0400

    sending message on update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,21 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
+// setup a function which will push out a message notifying the DB has updated
+export const onAdminChange = functions.firestore.document('data/users').onUpdate(change => {
+    const after = change.after.data();
+    // handle the message to be sent
+    const payload = {
+        data: {
+            admin: String(after?.admin),
+            name: after?.name
+        }
+    }
+    return admin.messaging().sendToTopic('data_users', payload);
+
+})
+
+// setup endpoint to GET
 export const getAdmin = functions.https.onRequest((request, response) => {
     admin.firestore().doc('data/users').get().then(snapshot => {
         const data = snapshot.data();
